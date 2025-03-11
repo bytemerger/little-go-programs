690bd5758e0f65335a327bb0508d11e5ed296593
francis <onyishif@gmail.com>
2025-03-11 22:38:48 +0100

    simple tic tac toe golang

---
+++ db.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"sync"
+)
+
+func db() {
+	mu := sync.RWMutex{}
+	memory := make(map[string]string)
+
+	router := http.NewServeMux()
+
+	router.HandleFunc("/get", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("processing a get request")
+		key := r.URL.Query().Get("key")
+		if key == "" {
+			http.Error(w, "Please pass a valid key", http.StatusBadRequest)
+			return
+		}
+
+		mu.RLock()
+		val, ok := memory[key]
+		mu.RUnlock()
+
+		if ok {
+			fmt.Fprintf(w, "%s", val)
+			return
+		}
+
+		http.Error(w, "Key not found", http.StatusNotFound)
+	})
+	router.HandleFunc("/set", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("processing a set request")
+
+		queryParams := r.URL.Query()
+		// only sets the first query param
+		var key, value string
+		for queryKey, queryValue := range queryParams {
+			key, value = queryKey, queryValue[0]
+			break
+		}
+		if len(key) < 0 || len(value) < 0 {
+			http.Error(w, "Malformed request", http.StatusBadRequest)
+			return
+		}
+
+		mu.Lock()
+		memory[key] = value
+		mu.Unlock()
+
+		fmt.Fprintf(w, "The key %s has been set in the memory\n", key)
+	})
+
+	fmt.Println("Server starting at port 4000")
+	err := http.ListenAndServe(":4000", router)
+	if err != nil {
+		fmt.Println("Error starting server ", err)
+	}
+}
--- main.go
+++ main.go
@@ -1,61 +1,142 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"net/http"
-	"sync"
+	"os"
+	"os/exec"
+	"time"
 )
 
 func main() {
-	mu := sync.RWMutex{}
-	memory := make(map[string]string)
+	// build a tic tac toe game
+	gameState := make([]string, 9)
+	playerTurn := 1
+	fmt.Printf("Welcome to tic tac toe in the terminal\n")
+	fmt.Printf("Below is the game layout\n\n")
+	displayGamePosition()
+	fmt.Printf("\nwe will expect each player to enter the position they want to play")
+	time.Sleep(time.Second * 4)
+	clearStdout()
 
-	router := http.NewServeMux()
+	for i := 0; i < 9; i++ {
+		fmt.Printf("player %d, enter your preferred position: ", playerTurn)
 
-	router.HandleFunc("/get", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("processing a get request")
-		key := r.URL.Query().Get("key")
-		if key == "" {
-			http.Error(w, "Please pass a valid key", http.StatusBadRequest)
-			return
+		input := getPlayerInput(gameState)
+
+		if playerTurn == 1 {
+			gameState[input-1] = "X"
+		} else {
+			gameState[input-1] = "O"
 		}
 
-		mu.RLock()
-		val, ok := memory[key]
-		mu.RUnlock()
+		// display the current game state and the position just inserted
 
-		if ok {
-			fmt.Fprintf(w, "%s", val)
+		displayGameState(gameState)
+
+		fmt.Printf("Player %d played position %d\n", playerTurn, input)
+
+		// check winner
+		isWin := checkWinner(gameState, playerTurn)
+		if isWin {
+			fmt.Printf("Player %d won \n", playerTurn)
 			return
+		} else {
+			time.Sleep(time.Second * 2)
+			clearStdout()
+			if playerTurn == 1 {
+				playerTurn = 2
+			} else {
+				playerTurn = 1
+			}
 		}
 
-		http.Error(w, "Key not found", http.StatusNotFound)
-	})
-	router.HandleFunc("/set", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("processing a set request")
+	}
+	fmt.Println("game ended in a draw")
 
-		queryParams := r.URL.Query()
-		// only sets the first query param
-		var key, value string
-		for queryKey, queryValue := range queryParams {
-			key, value = queryKey, queryValue[0]
+}
+
+func displayGamePosition() {
+	for i := 0; i < 9; i += 3 {
+		fmt.Printf(" %d  |  %d  |  %d\n", i+1, i+2, i+3)
+		if i < 6 {
+			fmt.Println("--- | --- | ---")
+		}
+	}
+
+}
+
+func getDisplayValue(value string) string {
+	if value == "" {
+		return "-"
+	}
+	return value
+}
+
+func displayGameState(state []string) {
+	for i := 0; i < 9; i += 3 {
+		fmt.Printf(" %s  |  %s  |  %s\n",
+			getDisplayValue(state[i]),
+			getDisplayValue(state[i+1]),
+			getDisplayValue(state[i+2]),
+		)
+		if i < 6 {
+			fmt.Println("--- | --- | ---")
+		}
+	}
+
+}
+
+func clearStdout() {
+	cmd := exec.Command("clear")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+
+func getPlayerInput(gameState []string) int {
+	var input int
+	stdin := bufio.NewReader(os.Stdin)
+
+	for {
+		_, _ = fmt.Fscan(stdin, &input)
+		if input > 0 && input < 10 {
+			if gameState[input-1] != "" {
+				clearStdout()
+				fmt.Print("Sorry invalid position, please enter a valid input: ")
+				continue
+			}
 			break
 		}
-		if len(key) < 0 || len(value) < 0 {
-			http.Error(w, "Malformed request", http.StatusBadRequest)
-			return
-		}
+		// clear the remaing things in the buffer to prevent infinite looop
+		stdin.ReadString('\n')
 
-		mu.Lock()
-		memory[key] = value
-		mu.Unlock()
-
-		fmt.Fprintf(w, "The key %s has been set in the memory\n", key)
-	})
-
-	fmt.Println("Server starting at port 4000")
-	err := http.ListenAndServe(":4000", router)
-	if err != nil {
-		fmt.Println("Error starting server ", err)
+		clearStdout()
+		fmt.Print("Sorry invalid position, please enter a valid input: ")
 	}
+	return input
+}
+
+func checkWinner(gameState []string, lastPlayer int) bool {
+	winPositions := [][]int{
+		{0, 1, 2},
+		{3, 4, 5},
+		{6, 7, 8},
+		{0, 3, 6},
+		{1, 4, 7},
+		{2, 5, 8},
+		{0, 4, 8},
+		{2, 4, 6},
+	}
+	var lastPlayerString string
+	if lastPlayer == 1 {
+		lastPlayerString = "X"
+	} else {
+		lastPlayerString = "O"
+	}
+	for _, winArr := range winPositions {
+		if gameState[winArr[0]] == lastPlayerString && gameState[winArr[1]] == lastPlayerString && gameState[winArr[2]] == lastPlayerString {
+			return true
+		}
+	}
+	return false
 }
